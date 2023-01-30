13b30efae200d0855aa1f172ddc46fc4a256ca21
Fatih Mehmet Baydar <baydarmf@gmail.com>
2023-01-30 08:55:56 +0300

    IActionResult Index added

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,13 +7,22 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    /*public string Index()
     {
         return "This is my default action...";
+    }*/
+    public IActionResult Index()
+    {
+        return View();
     }
     // 
     // GET: /HelloWorld/Welcome/ 
     // Requires using System.Text.Encodings.Web;
+    /*public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+    */
     public string Welcome(string name, int ID = 1)
     {
         return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
